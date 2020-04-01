c797ddca830802446582392071c082815cc5094f
Bryn <thenazboz@gmail.com>
2020-04-01 10:38:03 +0100

    Done.

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -10,9 +10,16 @@ class Pancake extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.startInterval()
+  }
   // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
 
   // TODO: create a componentWillUnmount() which will clear the interval
+
+  componentWillUnmount() {
+    this.cleanUpInterval()
+  }
 
   updateCounter = () => {
     this.setState({
@@ -73,8 +80,8 @@ class Pancake extends React.Component {
             {firstSide ? (
               <button onClick={this.flip}>Flip me!</button>
             ) : (
-              <button onClick={this.takeItOff}>Take me off!</button>
-            )}
+                <button onClick={this.takeItOff}>Take me off!</button>
+              )}
           </div>
         </div>
       </div>
